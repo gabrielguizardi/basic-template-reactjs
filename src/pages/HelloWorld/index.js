21dffcb5d418edf23ba1c5d5aa7802c7f72eaf00
Gabriel Guizardi Cardoso <gabrielguicar5@gmail.com>
2020-04-27 17:36:50 -0300

    Initial commit

---
+++ src/pages/HelloWorld/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function HelloWorld() {
+  return (
+    <div className="container">
+      <h1>Hello World :)</h1>
+      <p>Basic template React</p>
+    </div>
+  );
+}
